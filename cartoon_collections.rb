7105531d9d9de44ce8bcac0690619148cbcdb8f3
Camiyah Goins <tokyonote9@gmail.com>
2020-02-22 13:17:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(dwarves)
-  dwarves
+  dwarves dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+end
 end
 
 def summon_captain_planet# code an argument here
